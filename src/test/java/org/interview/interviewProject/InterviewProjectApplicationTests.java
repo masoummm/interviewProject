36fd4e102ff9194ff75896bd28330fcb4427bf4e
Masoumeh.Rasouli <masoumeh.rasouli@sekke.local>
2022-10-29 12:38:49 +0330

    creation interview project

---
+++ src/test/java/org/interview/interviewProject/InterviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.interview.interviewProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
